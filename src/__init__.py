2f73b05d705a6d62705186f855254cd133707bba
Vicky Kwan <vickykwan@VK-MBP15-2018.local>
2019-10-31 20:48:36 -0700

    added bugs log

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+#  bugs:
+# sf__opportunities_history: not found
+# stripe_charges: not overriden by explore sql name
+# finance_revenue_forecast: did not exclude comments 
+# liquid sql table name: sf__campaign_members, sf__opportunity_contact_role, sf__contacts, sf__campaigns,
+# sf__lead_became_suspect_date: NDT, needs to wait until all others are propogated
